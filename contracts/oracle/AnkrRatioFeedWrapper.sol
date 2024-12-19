0f5381e1f2bb29d5611288297c468ef1b29da291
RozalenokM <deathwing00000@gmail.com>
2024-12-19 17:26:25 +0700

    feat: ankrFlow oracle changes

--- contracts/oracle/AnkrRatioFeedWrapper.sol
+++ contracts/oracle/AnkrRatioFeedWrapper.sol
@@ -7,24 +7,25 @@ import {AggregatorV2V3Interface} from './interfaces/AggregatorV2V3Interface.sol'
 /**
  * @title A port of the ChainlinkAggregatorV3 interface that supports AnkrRationFeed
  * @notice This does not store any roundId information on-chain. Please review the code before using this implementation.
- * This smart contract supposed to behave like a Chainlink's oracle and return the price of ankrFlow/Flow
+ * This smart contract supposed to behave like a Chainlink's oracle and return the price of ankrFlow/usd
  */
-contract AnkrRatioFeedWrapper is AggregatorV2V3Interface {
+contract AnkrFlowToUsdFeed is AggregatorV2V3Interface {
   IInternetBondRatioFeed public ankrRatioFeed;
   address public ankrFlow;
+  AggregatorV2V3Interface public flowFeed;
 
-  constructor(address _ankrRatioFeed, address _ankrFlow) {
+  constructor(address _ankrRatioFeed, address _ankrFlow, address _flowFeed) {
     ankrRatioFeed = IInternetBondRatioFeed(_ankrRatioFeed);
     ankrFlow = _ankrFlow;
+    flowFeed = AggregatorV2V3Interface(_flowFeed);
   }
 
   function decimals() public view virtual returns (uint8) {
-    return uint8(18);
+    return uint8(8);
   }
 
   function description() public pure returns (string memory) {
-    return
-      "A wrapper of AnkrRatioFeed SC to support Chainlink's aggregator interface. Sets floor price of ankrFlow as 1:1 to wFlow";
+    return 'Feed that provides price of the AnkrFlow in USD';
   }
 
   function version() public pure returns (uint256) {
@@ -32,7 +33,7 @@ contract AnkrRatioFeedWrapper is AggregatorV2V3Interface {
   }
 
   function latestAnswer() public view virtual returns (int256) {
-    int256 answer = int256(_getSafeRatioForAnkrFlow());
+    int256 answer = _getPriceOfAnkrFlowInUsd();
     return answer;
   }
 
@@ -55,12 +56,12 @@ contract AnkrRatioFeedWrapper is AggregatorV2V3Interface {
   function getRoundData(
     uint80 _roundId
   ) external view returns (uint80, int256, uint256, uint256, uint80) {
-    int256 answer = int256(_getSafeRatioForAnkrFlow());
+    int256 answer = _getPriceOfAnkrFlowInUsd();
     return (_roundId, answer, latestTimestamp(), latestTimestamp(), _roundId);
   }
 
   function latestRoundData() external view returns (uint80, int256, uint256, uint256, uint80) {
-    int256 answer = int256(_getSafeRatioForAnkrFlow());
+    int256 answer = _getPriceOfAnkrFlowInUsd();
     return (
       uint80(latestTimestamp()),
       answer,
@@ -74,5 +75,28 @@ contract AnkrRatioFeedWrapper is AggregatorV2V3Interface {
     ratio = ankrRatioFeed.getRatioFor(ankrFlow);
 
     if (ratio > 1e18) ratio = 1e18;
+  }
+
+  function _getPriceOfAnkrFlowInUsd() internal view returns (int256) {
+    int256 flowAnkrFlowPrice = int256(_getSafeRatioForAnkrFlow());
+    int256 flowPriceInUsd = flowFeed.latestAnswer();
+
+    // Since AnkrRatioFeed returns price of 1 FLOW in AnkrFlow, we have to convert it.
+    // Price of AnkrFlow in FLOW is
+    //
+    // ankrFlowFlowPrice = 1 / flowAnkrFlowPrice
+    //
+    // Then to convert it to USD we need to multiply it with price of FLOW in USD
+    //
+    // flowPriceInUsd * ankrFlowFlowPrice
+    //
+    // or
+    //
+    // flowPriceInUsd / flowAnkrFlowPrice
+
+    // Decimal of flowPriceInUsd is 8, that means that we need to convert it to 18 decimal
+    // by multiplying by 10**(18 - 8) and then multiply by 10**8 to safe precision and keep
+    // decimal of usd.
+    return (flowPriceInUsd * 10 ** 10 * 10 ** 8) / flowAnkrFlowPrice;
   }
 }
